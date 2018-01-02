f6832169cac83641871cc082302b794884689cc4
deadeye64 <deadeye64@users.noreply.github.com>
2018-01-02 19:23:58 +0100

    Working on nenc (name encoder) support

--- src/deadfs.c
+++ src/deadfs.c
@@ -47,3 +47,53 @@ void dfs_del_file(struct dfs_file *file)
 	free((void*)file->appath);
 	free(file);
 }
+
+void dfs_add_nenc_fast(struct dfs_context *ctx, struct dfs_nenc_ops *ops, int id)
+{
+	struct dfs_nenc *nenc = NULL;
+
+
+	nenc = calloc(1, sizeof(struct dfs_nenc));
+
+	nenc->id = id;
+	nenc->ops = *ops;
+
+	HASH_ADD_INT(ctx->nencs, id, nenc);
+}
+
+static struct dfs_nenc* get_cur_nenc(struct dfs_context *ctx)
+{
+	struct dfs_nenc *nenc = NULL;
+
+	HASH_FIND_INT(ctx->nencs, &ctx->nenc_id, nenc);
+
+	return nenc;
+}
+
+int dfs_nenc_calc_enclen(struct dfs_context *ctx, const char *in)
+{
+	struct dfs_nenc *nenc = get_cur_nenc(ctx);
+
+	return nenc->ops.calc_enclen(in, nenc->ctx);
+}
+
+int dfs_nenc_calc_declen(struct dfs_context *ctx, const char *in)
+{
+	struct dfs_nenc *nenc = get_cur_nenc(ctx);
+
+	return nenc->ops.calc_declen(in, nenc->ctx);
+}
+
+void dfs_nenc_encode(struct dfs_context *ctx, const char *in, char *out, int len)
+{
+	struct dfs_nenc *nenc = get_cur_nenc(ctx);
+
+	nenc->ops.encode(in, out, len, nenc->ctx);
+}
+
+void dfs_nenc_decode(struct dfs_context *ctx, const char *in, char *out, int len)
+{
+	struct dfs_nenc *nenc = get_cur_nenc(ctx);
+
+	nenc->ops.decode(in, out, len, nenc->ctx);
+}
