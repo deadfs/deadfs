5e9646c493993c86ad37e12c709bd79bce460195
deadeye64 <deadeye64@users.noreply.github.com>
2018-01-02 19:25:21 +0100

    Adding support for key in config

--- src/config.h
+++ src/config.h
@@ -3,9 +3,15 @@
 
 #include <stdint.h>
 
+#define DFS_MASTERKEY_LEN 32
+
 struct dfs_config {
 	uint8_t version;
 
+	unsigned char master_key[DFS_MASTERKEY_LEN];
+
+	char nenc_name[20];
+	char denc_name[20];
 };
 
 #endif /* SRC_CONFIG_H_ */
