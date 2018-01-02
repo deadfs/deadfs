bd7bb45ddf692d9ccc2b22b400249152e2f4c33f
deadeye64 <deadeye64@users.noreply.github.com>
2018-01-02 19:23:58 +0100

    Working on nenc (name encoder) support

--- src/deadfs.h
+++ src/deadfs.h
@@ -7,19 +7,33 @@
 #include "config.h"
 #include "file.h"
 
+#include "nenc.h"
 
 struct dfs_context {
+
+	struct dfs_config *config;
+
 	char *basepath;
 
 	struct dfs_file *files;
+
+	int nenc_id;
+	struct dfs_nenc *nencs;
+
+
 };
 
-// Planning to get the ctx from file->ctx
 int dfs_add_file(struct dfs_file *file);
-
 struct dfs_file* dfs_add_file_fast(struct dfs_context *ctx, FILE *fp, const char *vpath, const char *appath);
 void dfs_del_file(struct dfs_file *file);
-
 struct dfs_file* dfs_get_file(struct dfs_context *ctx, const char *vpath);
+
+void dfs_add_nenc_fast(struct dfs_context *ctx, struct dfs_nenc_ops *ops, int id);
+
+
+int dfs_nenc_calc_enclen(struct dfs_context *ctx, const char *in);
+int dfs_nenc_calc_declen(struct dfs_context *ctx, const char *in);
+void dfs_nenc_encode(struct dfs_context *ctx, const char *in, char *out, int len);
+void dfs_nenc_decode(struct dfs_context *ctx, const char *in, char *out, int len);
 
 #endif /* SRC_DEADFS_H_ */
